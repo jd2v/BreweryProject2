3dee8c63538d7141411abc4395efaafe85d2f4c8
zach-18 <67443071+zach-18@users.noreply.github.com>
2021-01-16 09:48:28 -0600

    updated sequel database 1.16.21

--- brewdb_v2.sql
+++ brewdb_v2.sql
@@ -1,6 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/ru0hro
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 Drop Table "Breweries";
 Drop Table "CountyDemographics";
 
@@ -11,6 +8,8 @@ CREATE TABLE "CountyDemographics" (
     "area_name" VARCHAR(255)   NOT NULL,
     "median_HHI_2019" INT   NOT NULL,
     "bach_deg_14to18" FLOAT   NOT NULL,
+	"Latitude" FLOAT   NOT NULL,
+    "Longitude" FLOAT   NOT NULL,
     CONSTRAINT "pk_CountyDemographics" PRIMARY KEY (
         "CountyID"
      )
@@ -21,9 +20,11 @@ Copy "CountyDemographics" (
     "Stabr",
     "area_name",
     "median_HHI_2019",
-    "bach_deg_14to18"
+    "bach_deg_14to18",
+	"Latitude",
+    "Longitude"
 	)
-	From 'C:\PZBrew\Clean_data\countydemographicsv1.csv' DELIMITER ',' CSV HEADER;
+	From 'C:\PZBrew\Clean_data\countydemographicsv1latlong.csv' DELIMITER ',' CSV HEADER;
 
 
 CREATE TABLE "Breweries" (
@@ -35,6 +36,8 @@ CREATE TABLE "Breweries" (
     "County" VARCHAR(255)   NOT NULL,
     "State" VARCHAR(255)   NOT NULL,
     "CountyID" INT   NOT NULL,
+	"BestList" BOOLEAN NOT NULL,
+	"LargestList" BOOLEAN NOT NULL,
     CONSTRAINT "pk_Breweries" PRIMARY KEY (
         "Index"
 		)
@@ -48,9 +51,11 @@ Copy "Breweries" (
     "Longitude",
     "County",
     "State",
-    "CountyID"
+    "CountyID",
+	"BestList",
+	"LargestList"
 	) 
-	From 'C:\PZBrew\Clean_data\breweries_v1.csv' DELIMITER ',' CSV HEADER;	
+	From 'C:\PZBrew\Clean_data\breweries_v2.csv' DELIMITER ',' CSV HEADER;	
 
 
 ALTER TABLE "Breweries" ADD CONSTRAINT "fk_Breweries_CountyID" FOREIGN KEY("CountyID")
