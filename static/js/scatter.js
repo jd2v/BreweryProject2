0ad6cdf401ec35510f5855b6ee6bde458a08948a
Justin DeVries <justin.debeesknees@gmail.com>
2021-01-20 16:33:00 -0600

    Updates scatterplot

--- static/js/scatter.js
+++ static/js/scatter.js
@@ -18,7 +18,7 @@ d3.csv("/Resources/FinalCountyDemographics.csv", function(data) {
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, 155000])
+    .domain([35000, 155000])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -26,27 +26,22 @@ d3.csv("/Resources/FinalCountyDemographics.csv", function(data) {
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 90])
+    .domain([10, 80])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
-  // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
-  // Its opacity is set to 0: we don't see it by default.
+  // Adds a tooltip div
   var tooltip = d3.select("#scatter")
     .append("div")
     .style("opacity", 0)
     .attr("class", "tooltip")
-    .style("background-color", "white")
+    .style("background-color",  "rgb(102, 207, 88)")
     .style("border", "solid")
     .style("border-width", "1px")
     .style("border-radius", "5px")
     .style("padding", "10px")
-
-
-
-  // A function that change this tooltip when the user hover a point.
-  // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+  // A function that changes this tooltip when the user hover a point.
   var mouseover = function(d) {
     tooltip
       .style("opacity", 1)
@@ -54,19 +49,10 @@ d3.csv("/Resources/FinalCountyDemographics.csv", function(data) {
 
   var mousemove = function(d) {
     tooltip
-      .html("The area is " + d.area_name + " and it has " + d.Brewery_Count + " Breweries")
-      .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .html("The area is " + d.area_name + " and it has " + Math.floor(d.Brewery_Count) + " Breweries")
+      .style("left", (d3.mouse(this)[0]) + "px")
       .style("top", (d3.mouse(this)[1]) + "px")
   }
-
-  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
-  var mouseleave = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
-
   // Add dots
   svg.append('g')
     .selectAll("dot")
@@ -81,6 +67,4 @@ d3.csv("/Resources/FinalCountyDemographics.csv", function(data) {
       .style("stroke", "white")
     .on("mouseover", mouseover )
     .on("mousemove", mousemove )
-    .on("mouseleave", mouseleave )
-
 })
